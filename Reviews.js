a279e4927b17df41f66c665d02604fb515aa5608
paulnguyen576 <paulnguyen576@gmail.com>
2026-04-05 23:29:52 -0600

    pushing for render

--- Reviews.js
+++ Reviews.js
@@ -1,12 +1,18 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect(process.env.DB);
+const DB_URI = process.env.MONGODB_URI || process.env.DB;
+if (!mongoose.connection.readyState) {
+    mongoose.connect(DB_URI).catch(() => {});
+}
 
 // Movie schema
 var ReviewSchema = new Schema({
-
-});
+    movieId: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie', required: true },
+    username: { type: String, required: true },
+    review: { type: String, required: true },
+    rating: { type: Number, min: 0, max: 5, required: true }
+}, { collection: 'Reviews' });
 
 // return the model
 module.exports = mongoose.model('Review', ReviewSchema);
